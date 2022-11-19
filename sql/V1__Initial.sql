476b1631dbce477af31c42d213d022c3bf13abe3
Nick Darlow <dangerous.darlow@gmail.com>
2022-11-19 10:23:10 +0000

    Add tags table

--- sql/V1__Initial.sql
+++ sql/V1__Initial.sql
@@ -1,5 +1,6 @@
+-- transactions --
+
 CREATE TABLE transactions (
-    id bigint NOT NULL,
     uuid uuid NOT NULL,
     "timestamp" timestamp with time zone NOT NULL,
     amount bigint NOT NULL,
@@ -8,16 +9,11 @@ CREATE TABLE transactions (
     account text NOT NULL
 );
 
-CREATE SEQUENCE transactions_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+ALTER TABLE ONLY transactions ADD CONSTRAINT transactions_pk PRIMARY KEY (uuid);
 
-ALTER TABLE ONLY transactions ALTER COLUMN id SET DEFAULT nextval('transactions_id_seq'::regclass);
+CREATE UNIQUE INDEX transactions_uuid_uindex ON transactions USING btree (uuid);
 
-ALTER TABLE ONLY transactions ADD CONSTRAINT transactions_pk PRIMARY KEY (id);
+CREATE INDEX transactions_timestamp_index ON transactions USING btree ("timestamp" DESC);
 
 CREATE INDEX transactions_amount_index ON transactions USING btree (amount);
 
@@ -25,9 +21,22 @@ CREATE INDEX transactions_description_index ON transactions USING btree (descrip
 
 CREATE UNIQUE INDEX transactions_timestamp_amount_description_uindex ON transactions USING btree ("timestamp", amount, description);
 
-CREATE INDEX transactions_timestamp_index ON transactions USING btree ("timestamp" DESC);
 
-CREATE UNIQUE INDEX transactions_uuid_uindex ON transactions USING btree (uuid);
+-- tags --
+
+CREATE TABLE tags (
+    uuid uuid NOT NULL,
+    name text NOT NULL
+);
+
+ALTER TABLE ONLY tags ADD CONSTRAINT tags_pk PRIMARY KEY (uuid);
+
+CREATE UNIQUE INDEX tags_uuid_uindex ON tags USING btree (uuid);
+
+CREATE UNIQUE INDEX tags_name_uindex ON tags USING btree (name);
+
+
+-- functions --
 
 CREATE FUNCTION readable(bigint) RETURNS numeric
 	LANGUAGE SQL
